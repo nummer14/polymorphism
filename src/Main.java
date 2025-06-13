ce71bd1771108f99ebe22eaa5db7761910f86c63
numme <nummer14@naver.com>
2025-06-13 14:24:18 +0900

    Fourth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,17 @@
-abstract class Shape {
-    public abstract double area();
+interface Animal {
+    void makeSound();
 }
 
-class Circle extends Shape {
-    private double r;
-
-    public Circle(double r) {
-        this.r = r;
-    }
-
+class Dog implements Animal {
     @Override
-    public double area() {
-        return Math.PI * r * r;
+    public void makeSound() {
+        System.out.println("Bow-wow!");
     }
 }
-
-class Rectangle extends Shape {
-    private double b, h;
-
-    public Rectangle(double b, double h) {
-        this.b = b;
-        this.h = h;
-    }
-    @Override
-    public double area() {
-        return b * h;
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
+        Animal dog = new Dog();
 
-        shapes[0] = new Circle(3);
-        shapes[1] = new Rectangle(4, 5);
-
-        for (Shape shape : shapes) {
-            System.out.println(shape.area());
-        }
+        dog.makeSound();
     }
 }
